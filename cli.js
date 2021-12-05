1308ac3e705f90c3570fa14f47d0ab1fae97edc0
Yongjun Rong <rongyj@hotmail.com>
2021-12-05 12:40:15 -0500

    Add Yotascale

--- cli.js
+++ cli.js
@@ -19,250 +19,251 @@ const puppeteer = require('puppeteer');
 const btoa = require('btoa');
 
 program
-	.version(pkg.version)
+    .version(pkg.version)
 
 program
-	.command('export <path_json> [resume_gen_tags] [template_location] [pdf_location] [css_file_location]')
-	.description('Export a pdf resume from the json resume provided to the given location, applying the css file given')
-	.action(function (path_json, resume_gen_tags, temp_location, pdf_location, css_file_location) {
-		createHtml(path_json, resume_gen_tags, temp_location, css_file_location, function (err, html) {
-			if (err) {
-				console.log(err);
-				process.exit(1);
-			}
-			createPdf(html, pdf_location, (error) => {
-				if (error) {
-					console.error(error, '`createPdf` errored out');
-					callback(error);
-				}
-			});
-		});
+    .command('export <path_json> [resume_gen_tags] [template_location] [pdf_location] [css_file_location]')
+    .description('Export a pdf resume from the json resume provided to the given location, applying the css file given')
+    .action(function(path_json, resume_gen_tags, temp_location, pdf_location, css_file_location) {
+        createHtml(path_json, resume_gen_tags, temp_location, css_file_location, function(err, html) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            createPdf(html, pdf_location, (error) => {
+                if (error) {
+                    console.error(error, '`createPdf` errored out');
+                    callback(error);
+                }
+            });
+        });
 
-	});
+    });
 
 const createPdf = (html, pdf_location, callback) => {
-	(async () => {
-		const puppeteerLaunchArgs = [];
+    (async() => {
+        const puppeteerLaunchArgs = [];
 
-		if (process.env.RESUME_PUPPETEER_NO_SANDBOX) {
-			puppeteerLaunchArgs.push('--no-sandbox');
-		}
+        if (process.env.RESUME_PUPPETEER_NO_SANDBOX) {
+            puppeteerLaunchArgs.push('--no-sandbox');
+        }
 
 
-		const browser = await puppeteer.launch({
-			args: puppeteerLaunchArgs,
-			//headless:false,
-			//product: 'chrom',
-			//executablePath: "/Applications/Firefox Nightly.app/Contents/MacOS/firefox",
-			//executablePath: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
-		});
-		const page = await browser.newPage();
+        const browser = await puppeteer.launch({
+            args: puppeteerLaunchArgs,
+            //headless:false,
+            //product: 'chrom',
+            //executablePath: "/Applications/Firefox Nightly.app/Contents/MacOS/firefox",
+            //executablePath: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
+        });
+        const page = await browser.newPage();
 
-		await page.emulateMediaType(
-			({ margin: '0 0 0 0', mediaType: 'print' } && 'print') ||
-			'screen',
-		);
-		await page.goto(
-			`data:text/html;base64,${btoa(unescape(encodeURIComponent(html)))}`,
-			{ waitUntil: 'networkidle0' },
-		);
+        await page.emulateMediaType(
+            ({ margin: '0 0 0 0', mediaType: 'print' } && 'print') ||
+            'screen',
+        );
+        await page.goto(
+            `data:text/html;base64,${btoa(unescape(encodeURIComponent(html)))}`, { waitUntil: 'networkidle0' },
+        );
 
-		var finalPdfLocation = '/resume.pdf';
+        var finalPdfLocation = '/resume.pdf';
 
-		if (pdf_location) {
-			finalPdfLocation = '/' + pdf_location;
-		}
-		var _path = process.cwd() + finalPdfLocation;
+        if (pdf_location) {
+            finalPdfLocation = '/' + pdf_location;
+        }
+        var _path = process.cwd() + finalPdfLocation;
 
-		await page.pdf({
-			path: _path,
-			format: 'Letter',
-			printBackground: true,
-			preferCSSPageSize: true,
-			margin: {
-				top: '50px',
-				bottom: '45px',
-				right: '40px',
-			},
-			displayHeaderFooter: true,
-			headerTemplate: "<div/>",
-			footerTemplate: "<table style=\"width:100%; font-size:6px; \"><tr><td style=\"width:33.333%;text-align:left;\"></td><td style=\"width:33.333%; text-align:center;vertical-align:bottom; \">Yongjun Rong</td><td style=\"width:33.333%; text-align:right;vertical-align:bottom; padding-right:20px;\" class=\"pageNumber\"></td></tr></table>",
-		});
+        await page.pdf({
+            path: _path,
+            format: 'Letter',
+            printBackground: true,
+            preferCSSPageSize: true,
+            margin: {
+                top: '50px',
+                bottom: '45px',
+                right: '40px',
+            },
+            displayHeaderFooter: true,
+            headerTemplate: "<div/>",
+            footerTemplate: "<table style=\"width:100%; font-size:6px; \"><tr><td style=\"width:33.333%;text-align:left;\"></td><td style=\"width:33.333%; text-align:center;vertical-align:bottom; \">Yongjun Rong</td><td style=\"width:33.333%; text-align:right;vertical-align:bottom; padding-right:20px;\" class=\"pageNumber\"></td></tr></table>",
+        });
 
-		console.log("pdf file is created at " + _path);
+        console.log("pdf file is created at " + _path);
 
-		await browser.close();
-	})()
-		.then(callback)
-		.catch(callback);
+        await browser.close();
+    })()
+    .then(callback)
+        .catch(callback);
 
 };
 
 function createHtml(path_json, resume_gen_tags, temp_location, css_file_location, callback) {
-	var template_location = __dirname + "/templates/" + "resume.tpl";
-	if (temp_location)
-		template_location = temp_location;
-	fs.readFile(template_location, 'utf-8', function (err, data) {
-		if (err) {
-			console.log(err);
-			return callback(err, null);
-			//process.exit(1);
-		} else {
-			var templateContent = data;
-			fs.readFile(__dirname + '/' + path_json, 'utf-8', function (err, data) {
-				if (err) {
-					console.log(err);
-					return callback(err, null);
-					//process.exit(1);
-				}
-				var resumeJson = JSON.parse(data);
-				var originalResumeJson = JSON.parse(data);
-				var v = verifier.run(resumeJson);
+    var template_location = __dirname + "/templates/" + "resume.tpl";
+    if (temp_location)
+        template_location = temp_location;
+    fs.readFile(template_location, 'utf-8', function(err, data) {
+        if (err) {
+            console.log(err);
+            return callback(err, null);
+            //process.exit(1);
+        } else {
+            var templateContent = data;
+            fs.readFile(__dirname + '/' + path_json, 'utf-8', function(err, data) {
+                if (err) {
+                    console.log(err);
+                    return callback(err, null);
+                    //process.exit(1);
+                }
+                var resumeJson = JSON.parse(data);
+                var originalResumeJson = JSON.parse(data);
+                var v = verifier.run(resumeJson);
 
-				if (templateContent && v) {
-					var _cssFile = "/static/css/base.css";
-					if (css_file_location) {
-						_cssFile = '/' + css_file_location;
-					}
+                if (templateContent && v) {
+                    var _cssFile = "/static/css/base.css";
+                    if (css_file_location) {
+                        _cssFile = '/' + css_file_location;
+                    }
 
-					fs.readFile(__dirname + _cssFile, 'utf-8', function (err, data) {
-						if (err) {
-							console.log(err);
-							return callback(err, null);
-							//process.exit(1);
-						}
+                    fs.readFile(__dirname + _cssFile, 'utf-8', function(err, data) {
+                        if (err) {
+                            console.log(err);
+                            return callback(err, null);
+                            //process.exit(1);
+                        }
 
-						data += "#extra {display: none;}"
+                        data += "#extra {display: none;}"
 
-						var head = "<head><style>" + data + "</style></head>";
-						templateContent = head + templateContent;
+                        var head = "<head><style>" + data + "</style></head>";
+                        templateContent = head + templateContent;
 
-						resumeJson.resume["original"] = originalResumeJson.resume;
-						if (resume_gen_tags == "short") {
-							utils.removeProjectsHighlights(resumeJson);
-						} else if (resume_gen_tags == "USA") {
-							utils.removeNonUSACompanies(resumeJson);
-						}
-						else if (resume_gen_tags == "FullStack") {
-							var expectTags = ["Java", "JavaScript", "FullStack"];
-							utils.filterProjects(resumeJson, expectTags);
-						} else if (resume_gen_tags == "table") {
-							delete resumeJson.resume.work;
-							delete resumeJson.resume.hobbies["hobby-projects"];
-						} else if (resume_gen_tags && resume_gen_tags != "full") {
-							var expectTags = resume_gen_tags.split(',');
-							utils.filterProjects(resumeJson, expectTags);
-						}
-						var html = mustache.to_html(templateContent, { "resume": resumeJson.resume });
-						callback(null, html);
-					});
-				}
-			});
-		}
-	});
+                        resumeJson.resume["original"] = originalResumeJson.resume;
+                        if (isNaN(resume_gen_tags)) {
+                            utils.removeProjectsHighlights(resumeJson, resume_gen_tags);
+                        } else if (resume_gen_tags == "short") {
+                            utils.removeProjectsHighlights(resumeJson);
+                        } else if (resume_gen_tags == "USA") {
+                            utils.removeNonUSACompanies(resumeJson);
+                        } else if (resume_gen_tags == "FullStack") {
+                            var expectTags = ["Java", "JavaScript", "FullStack"];
+                            utils.filterProjects(resumeJson, expectTags);
+                        } else if (resume_gen_tags == "table") {
+                            delete resumeJson.resume.work;
+                            delete resumeJson.resume.hobbies["hobby-projects"];
+                        } else if (resume_gen_tags && resume_gen_tags != "full") {
+                            var expectTags = resume_gen_tags.split(',');
+                            utils.filterProjects(resumeJson, expectTags);
+                        }
+                        var html = mustache.to_html(templateContent, { "resume": resumeJson.resume });
+                        callback(null, html);
+                    });
+                }
+            });
+        }
+    });
 }
 
 program
-	.command('exportToHtml <path_json> [resume_gen_tags] [template_location] [html_location] [css_file_location] ')
-	.description('Export an html resume from the json resume provided to the given location,' +
-		'applying the css file given, also will choose the right projects based on the specified tags' +
-		'Example: exportToHtml ./resume-schema.json full|short|[any tags defined in the projects]' +
-		'Example: ./cli.js exportToHtml ./resume-schema.json short' +
-		'Example: ./cli.js exportToHtml ./resume-schema.json USA' +
-		'Example: ./cli.js exportToHtml ./resume-schema.json devops' +
-		'Example: ./cli.js exportToHtml ./resume-schema.json Java' +
-		'Example: ./cli.js exportToHtml ./resume-schema.json FullStack' +
-		'Example: ./cli.js exportToHtml ./resume-schema.json full ./templates/table.tpl')
-	.action(function (path_json, resume_gen_tags, temp_location, html_location, css_file_location) {
-		createHtml(path_json, resume_gen_tags, temp_location, css_file_location, function (err, html) {
-			if (err) {
-				console.log(err);
-				process.exit(1);
-			}
-			var outputLocation = '/resume.html';
+    .command('exportToHtml <path_json> [resume_gen_tags] [template_location] [html_location] [css_file_location] ')
+    .description('Export an html resume from the json resume provided to the given location,' +
+        'applying the css file given, also will choose the right projects based on the specified tags' +
+        'Example: exportToHtml ./resume-schema.json full|short|[any tags defined in the projects]' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json 2' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json short' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json USA' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json devops' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json Java' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json FullStack' +
+        'Example: ./cli.js exportToHtml ./resume-schema.json full ./templates/table.tpl')
+    .action(function(path_json, resume_gen_tags, temp_location, html_location, css_file_location) {
+        createHtml(path_json, resume_gen_tags, temp_location, css_file_location, function(err, html) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            var outputLocation = '/resume.html';
 
-			if (html_location) {
-				outputLocation = '/' + html_location;
-			}
+            if (html_location) {
+                outputLocation = '/' + html_location;
+            }
 
-			outputLocation = process.cwd() + outputLocation;
-			fs.writeFile(outputLocation, html, function (err) {
-				if (err) {
-					console.log(err);
-					process.exit(1);
-				}
-				console.log("HTML file is created at " + outputLocation);
-			});
+            outputLocation = process.cwd() + outputLocation;
+            fs.writeFile(outputLocation, html, function(err) {
+                if (err) {
+                    console.log(err);
+                    process.exit(1);
+                }
+                console.log("HTML file is created at " + outputLocation);
+            });
 
-		});
+        });
 
-	});
-
-program
-	.command('exportToPlainText <path_json>  <resume_gen_tags> [output_location]')
-	.description('Export a text file resume from the json resume provided to the given location' +
-		'Example: ./cli.js exportToPlainText ./resume-schema.json full')
-	.action(function (path_json, resume_gen_tags, output_location) {
-		fs.readFile(__dirname + '/' + path_json, 'utf-8', function (err, data) {
-			if (err) {
-				console.log(err);
-				process.exit(1);
-			}
-			var resumeJson = JSON.parse(data);
-			//var originalResumeJson = JSON.parse(data);
-			var v = verifier.run(resumeJson);
-			if (v) {
-				//resumeJson.resume["original"]=originalResumeJson.resume;
-				if (resume_gen_tags == "short") {
-					utils.removeProjectsHighlights(resumeJson);
-				} else if (resume_gen_tags == "FullStack") {
-					var expectTags = ["Java", "JavaScript", "FullStack"];
-					utils.filterProjects(resumeJson, expectTags);
-				} else if (resume_gen_tags == "table") {
-					delete resumeJson.resume.work;
-					delete resumeJson.resume.hobbies["hobby-projects"];
-				} else if (resume_gen_tags && resume_gen_tags != "full") {
-					var expectTags = resume_gen_tags.split(',');
-					utils.filterProjects(resumeJson, expectTags);
-				}
-
-				var outputLocation = '/resume.txt';
-
-				if (output_location) {
-					outputLocation = '/' + output_location;
-				}
-
-				outputLocation = process.cwd() + outputLocation;
-				var textFileContent = utils.generatePlainTextFromJson(resumeJson);
-				fs.writeFile(outputLocation, textFileContent, function (err) {
-					if (err) {
-						console.log(err);
-						process.exit(1);
-					}
-				});
-			}
-		});
-	});
+    });
 
 program
-	.command('generateFromJsonResume <path_json>  [output_location]')
-	.description('Generate a json from a json-resume file')
-	.action(function (path_json, output_location) {
-		fs.readFile(__dirname + '/' + path_json, 'utf-8', function (err, data) {
-			if (err) {
-				console.log(err);
-				process.exit(1);
-			}
+    .command('exportToPlainText <path_json>  <resume_gen_tags> [output_location]')
+    .description('Export a text file resume from the json resume provided to the given location' +
+        'Example: ./cli.js exportToPlainText ./resume-schema.json full')
+    .action(function(path_json, resume_gen_tags, output_location) {
+        fs.readFile(__dirname + '/' + path_json, 'utf-8', function(err, data) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            var resumeJson = JSON.parse(data);
+            //var originalResumeJson = JSON.parse(data);
+            var v = verifier.run(resumeJson);
+            if (v) {
+                //resumeJson.resume["original"]=originalResumeJson.resume;
+                if (resume_gen_tags == "short") {
+                    utils.removeProjectsHighlights(resumeJson);
+                } else if (resume_gen_tags == "FullStack") {
+                    var expectTags = ["Java", "JavaScript", "FullStack"];
+                    utils.filterProjects(resumeJson, expectTags);
+                } else if (resume_gen_tags == "table") {
+                    delete resumeJson.resume.work;
+                    delete resumeJson.resume.hobbies["hobby-projects"];
+                } else if (resume_gen_tags && resume_gen_tags != "full") {
+                    var expectTags = resume_gen_tags.split(',');
+                    utils.filterProjects(resumeJson, expectTags);
+                }
 
-			var asJsonResume = JSON.parse(data);
-			var generatedJson = converter.parseJsonResumeFormat(asJsonResume);
+                var outputLocation = '/resume.txt';
 
-			if (output_location) {
-				converter.writeGeneratedJsonToFile(__dirname + '/' + output_location, generatedJson);
-			} else {
-				converter.writeGeneratedJsonToFile('converted.json', generatedJson);
-			}
-		});
-	});
+                if (output_location) {
+                    outputLocation = '/' + output_location;
+                }
+
+                outputLocation = process.cwd() + outputLocation;
+                var textFileContent = utils.generatePlainTextFromJson(resumeJson);
+                fs.writeFile(outputLocation, textFileContent, function(err) {
+                    if (err) {
+                        console.log(err);
+                        process.exit(1);
+                    }
+                });
+            }
+        });
+    });
+
+program
+    .command('generateFromJsonResume <path_json>  [output_location]')
+    .description('Generate a json from a json-resume file')
+    .action(function(path_json, output_location) {
+        fs.readFile(__dirname + '/' + path_json, 'utf-8', function(err, data) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+
+            var asJsonResume = JSON.parse(data);
+            var generatedJson = converter.parseJsonResumeFormat(asJsonResume);
+
+            if (output_location) {
+                converter.writeGeneratedJsonToFile(__dirname + '/' + output_location, generatedJson);
+            } else {
+                converter.writeGeneratedJsonToFile('converted.json', generatedJson);
+            }
+        });
+    });
 
 program.parse(process.argv);
---
+++ templates/resume-3pages.tpl
@@ -0,0 +1,123 @@
+<div class="container">
+    <div id="cv">
+            <div id="contact" {{#resume.contact.id}} data-id="{{ resume.contact.id }}" {{/resume.contact.id}}>
+            <div class="contact-name">{{ resume.contact.first_name }} {{ resume.contact.last_name }}</div>
+            <div class="contact-email">
+              <span class="contact-email">{{ resume.contact.city }} ({{ resume.contact.visa}})</span>
+              <span class="header-date"> Email: </span>
+              <span class="header-school-location">  {{ resume.contact.email }} </span>
+              <span class="header-date"> Tel: </span>
+              <span class="header-school-location">  {{ resume.contact.phone }} </span>
+            </div>
+            <div class="contact-website">
+                  <span class="header-date"> Linkedin: </span>
+                  <span class="header-school-location"> {{ resume.contact.website }},   </span>
+                  <span class="header-date"> Github: </span>
+                  <span class="header-school-location"> {{ resume.contact.github }} </span>
+            </div>
+        </div>
+
+        {{ #resume.education.length }}
+        <div id="education" class="section">
+            <div class="title-section">Education</div>
+            {{ #resume.education }}
+            <div class="education-item" {{#item-education.id}} data-id="{{ item-education.id }}" {{/item-education.id}}>
+                <span class="header-date">{{ item-education.end-date }},</span>
+                <span class="description-education">{{ item-education.description }},</span>
+                <span class="header-school-name">{{ item-education.institution.name }},</span>
+                <span class="header-school-location">{{ item-education.institution.city }}, {{ item-education.institution.country }}</span>
+            </div>
+            {{ /resume.education }}
+        </div>
+        {{ /resume.education.length }}
+        {{ #resume.summaries.length }}
+        <div id="skills" class="section">
+            <div class="title-section" style="page-break-after: avoid">Summary:</div>
+
+            <div>
+              <ul class="highlighted">
+                  {{ #resume.summaries }}
+                  <div class="description-work" style="page-break-inside: avoid"> {{ summary }} </div>
+                  <!--li>{{.}}</li-->
+                  {{ /resume.summaries}}
+              </ul>
+            </div>
+        </div>
+        {{ /resume.summaries.length }}
+
+        {{ #resume.skills.length }}
+        <div id="skills" class="section">
+            <div class="title-section" style="page-break-after: avoid">Skills:</div>
+            <div>
+              <ul class="highlighted">
+                  {{ #resume.skills }}
+                  <li>{{.}}</li>
+                  {{ /resume.skills}}
+              </ul>
+            </div>
+        </div>
+        {{ /resume.skills.length }}
+        {{ #resume.hobbies.hobby-items.length }}
+        <div id="hobbies" class="section" {{#resume.hobbies.id}} data-id="{{ resume.hobbies.id }}" {{/resume.hobbies.id}}>
+            <div class="title-section">Hobbies</div>
+            <div class="hobbies-items">
+                {{ #resume.hobbies.hobby-items.length }}
+                <ul class="highlighted">
+                  {{ #resume.hobbies.hobby-items}}
+                    <li>
+                      {{ item-hobbies.name}}
+                      {{ #item-hobbies.additional-info }}
+                          ({{ item-hobbies.additional-info }})
+                      {{ /item-hobbies.additional-info }}
+                    </li>
+                  {{/resume.hobbies.hobby-items}}
+                </ul>
+                {{ /resume.hobbies.hobby-items.length}}
+            </div>
+        </div>
+        {{ /resume.hobbies.hobby-items.length}}
+        {{ #resume.original.work.length}}
+        <div id="work-experience" class="section">
+          <div class="title-section" style="page-break-before: always;">Work Experience Summary:</div>
+          <div {{#item-work.id}} data-id="{{ item-work.id }}" {{/item-work.id}}>
+          <table id="work-experience" class="section" style="page-break-inside:auto;">
+            <tr class="work-item-header">
+                <th class="header-employer" style="width:20%">Employments</th>
+                <th class="header-accomplishments"> Major Accomplishments & Technologies </th>
+            </tr>
+            {{ #resume.original.work}}
+              <tr class="work-item" {{#item-work.id}} data-id="{{ item-work.id }}" {{/item-work.id}} style="page-break-before:auto;page-break-inside:avoid;page-break-after:avoid;">
+                <td class="work-employer" rowspan="2" style="page-break-inside:avoid;page-break-after:avoid;page-break-before:auto;">
+                  <p>{{ item-work.start-date}} &ndash; {{ item-work.end-date }}
+                  <p class="bold-font">{{ item-work.company.name }}
+                  <p>{{ item-work.company.city }},{{ item-work.company.country }}
+                  <p class="bold-font">{{ item-work.position }}
+                </td>
+                <td style="page-break-inside:avoid;page-break-after:avoid;page-break-before:auto;">
+                  {{#item-work.projects.length }}
+                  <ul class="highlighted">
+                    {{ #item-work.projects}}
+                      <li>{{item-projects.title}}</li>
+                    {{/item-work.projects}}
+                  </ul>
+                  {{/item-work.projects.length }}
+                </td>
+              </tr>
+              <tr style="page-break-inside:avoid;page-break-after:auto;page-break-before:avoid;">
+                <td style="page-break-inside:avoid;page-break-after:auto;page-break-before:avoid;">
+                  {{ #item-work.technologies.length }}
+                  <span class="technologies-work" style="page-break-inside:avoid;page-break-after:auto;page-break-before:avoid;">
+                      {{ #item-work.technologies }}
+                      {{ . }},
+                      {{ /item-work.technologies }}
+                  </span>
+                  {{ /item-work.technologies.length }}
+                </td>
+             </tr>
+             {{ /resume.original.work}}
+            </table>
+          </div>
+        </div>
+        {{ /resume.original.work.length}}
+    </div>
+</div>
